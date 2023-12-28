0318771b5b3359cf70e1051695e91435adc62cc3
isumi1357902 <41429695+isumi1357902@users.noreply.github.com>
2023-12-28 10:38:29 +0800

    f

--- dcu_updatedriver.psm1
+++ dcu_updatedriver.psm1
@@ -10,6 +10,16 @@ function dcu_updatedriver ([string]$para1 , [string]$para2 , [string]$para3 , [s
         $scriptRoot=$PSScriptRoot
     }
 
+    $actionss ="screenshot"
+    Get-Module -name $actionss|remove-module
+    $mdpath=(Get-ChildItem -path $scriptRoot -r -file | Where-Object {$_.name -match "^$actionss\b" -and $_.name -match "psm1"}).fullname
+    Import-Module $mdpath -WarningAction SilentlyContinue -Global
+
+    $actionsa ="startmenuapp"
+    Get-Module -name $actionsa|remove-module
+    $mdpath=(Get-ChildItem -path $scriptRoot -r -file | Where-Object {$_.name -match "^$actionsa\b" -and $_.name -match "psm1"}).fullname
+    Import-Module $mdpath -WarningAction SilentlyContinue -Global
+
     $tcpath=(Split-Path -Parent $scriptRoot)+"\currentjob\TC.txt"
     $tcnumber=((get-content $tcpath).split(","))[0]
     $tcstep=((get-content $tcpath).split(","))[1]
@@ -36,7 +46,6 @@ function dcu_updatedriver ([string]$para1 , [string]$para2 , [string]$para3 , [s
     $xmlObject.Manifest.SoftwareComponent.packageID = $para3
     $xmlObject.Manifest.SoftwareComponent.size = (Get-ChildItem $DUPPath).Length
 
-
     #Get DUP hash
     $hash_MD5 = Get-FileHash -Path $DUPPath -Algorithm MD5
     $hash_SHA256 = Get-FileHash -Path $DUPPath -Algorithm SHA256
@@ -46,9 +55,51 @@ function dcu_updatedriver ([string]$para1 , [string]$para2 , [string]$para3 , [s
     $xmlObject.Manifest.SoftwareComponent.Cryptography.Hash[1].'#text' = $hash_SHA256
     $xmlObject.Manifest.SoftwareComponent.Cryptography.Hash[2].'#text' = $hash_SHA1
 
-
     # Save the modified XML content to a new file
     $xmlObject.Save($XmlPath)
+
+    &$actionsa -para1 "dell command" -para3 "nonlog"
+
+    Start-Sleep -s 20
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB 9"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"+"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB 3"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait($XmlPath)
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB 6"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"-"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB 10"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"TAB 8"})
+    Start-Sleep -s 5
+    [System.Windows.Forms.SendKeys]::Sendwait({"ENTER"})
+
+    #4 + 4
+    #Get-Process NVMUP + DUP
+
+
+    #Loading XML
+    Start-Sleep -s 60
+    &$actionss -para3 "nonlog" -para5 "XMLFileLoad"
 
 
     ######### write log #######
