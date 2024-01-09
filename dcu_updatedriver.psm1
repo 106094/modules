5bce0c3222e0c027ff78cf7d0da3073390c81508
isumi1357902 <41429695+isumi1357902@users.noreply.github.com>
2024-01-09 16:53:32 +0800

    1123

--- dcu_updatedriver.psm1
+++ dcu_updatedriver.psm1
@@ -1,6 +1,75 @@
 function dcu_updatedriver ([string]$para1 , [string]$para2 , [string]$para3 , [string]$para4,[string]$para5){
-    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
+    Add-Type -AssemblyName Microsoft.VisualBasic
     Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Windows.Forms,System.Drawing 
+
+    $clickSource = @'
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+public class Clicker
+{
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct INPUT
+{ 
+    public int        type; // 0 = INPUT_MOUSE,
+                            // 1 = INPUT_KEYBOARD
+                            // 2 = INPUT_HARDWARE
+    public MOUSEINPUT mi;
+}
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646273(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct MOUSEINPUT
+{
+    public int    dx ;
+    public int    dy ;
+    public int    mouseData ;
+    public int    dwFlags;
+    public int    time;
+    public IntPtr dwExtraInfo;
+}
+
+//This covers most use cases although complex mice may have additional buttons
+//There are additional constants you can use for those cases, see the msdn page
+const int MOUSEEVENTF_MOVED      = 0x0001 ;
+const int MOUSEEVENTF_LEFTDOWN   = 0x0002 ;
+const int MOUSEEVENTF_LEFTUP     = 0x0004 ;
+const int MOUSEEVENTF_RIGHTDOWN  = 0x0008 ;
+const int MOUSEEVENTF_RIGHTUP    = 0x0010 ;
+const int MOUSEEVENTF_MIDDLEDOWN = 0x0020 ;
+const int MOUSEEVENTF_MIDDLEUP   = 0x0040 ;
+const int MOUSEEVENTF_WHEEL      = 0x0080 ;
+const int MOUSEEVENTF_XDOWN      = 0x0100 ;
+const int MOUSEEVENTF_XUP        = 0x0200 ;
+const int MOUSEEVENTF_ABSOLUTE   = 0x8000 ;
+
+const int screen_length = 0x10000 ;
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+[System.Runtime.InteropServices.DllImport("user32.dll")]
+extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+public static void LeftClickAtPoint(int x, int y)
+{
+    //Move the mouse
+    INPUT[] input = new INPUT[3];
+    input[0].mi.dx = x*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+    input[0].mi.dy = y*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+    input[0].mi.dwFlags = MOUSEEVENTF_MOVED | MOUSEEVENTF_ABSOLUTE;
+    //Left mouse button down
+    input[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    //Left mouse button up
+    input[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(3, input, Marshal.SizeOf(input[0]));
+}
+
+
+}
+'@
+Add-Type -TypeDefinition $clickSource -ReferencedAssemblies System.Windows.Forms,System.Drawing
 
     if($PSScriptRoot.length -eq 0){
         $scriptRoot="C:\testing_AI\modules"
@@ -86,9 +155,10 @@ function dcu_updatedriver ([string]$para1 , [string]$para2 , [string]$para3 , [s
     Start-Sleep -s 5
     [System.Windows.Forms.SendKeys]::Sendwait("{-}")
     Start-Sleep -s 5
-    [System.Windows.Forms.SendKeys]::Sendwait("{TAB 10}")
-    Start-Sleep -s 5
-    [System.Windows.Forms.SendKeys]::Sendwait("{ENTER}")
+    [Clicker3]::LeftClickAtPoint("60%", "70%")
+   # [System.Windows.Forms.SendKeys]::Sendwait("{TAB 10}")
+   # Start-Sleep -s 5
+   # [System.Windows.Forms.SendKeys]::Sendwait("{ENTER}")
     Start-Sleep -s 5
     [System.Windows.Forms.SendKeys]::Sendwait("{TAB 8}")
     Start-Sleep -s 5
