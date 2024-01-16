c541299b0584fdd92852834bf5ec884d56d012f9
LazyFinger <97865400+106094@users.noreply.github.com>
2024-01-16 10:10:58 +0800

    copyresult method modift

--- getresult.psm1
+++ getresult.psm1
@@ -2,7 +2,7 @@
 function getresult([string]$para1){
 
     Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
-    $wshell=New-Object -ComObject wscript.shell
+    #$wshell=New-Object -ComObject wscript.shell
       Add-Type -AssemblyName Microsoft.VisualBasic
       Add-Type -AssemblyName System.Windows.Forms
        Add-Type -AssemblyName System.Windows.Forms,System.Drawing
@@ -27,13 +27,13 @@ $scriptRoot=$PSScriptRoot
 $tcpath=(Split-Path -Parent $scriptRoot)+"\currentjob\TC.txt"
 $tcnumber=((get-content $tcpath).split(","))[0]
 $tcstep=((get-content $tcpath).split(","))[1]
-$logpath=(Split-Path -Parent $scriptRoot)+"\logs\$($tcnumber)\"
+$logpath=(Split-Path -Parent $scriptRoot)+"\logs\$($tcnumber)\$($tcstep)_$($result_type)_results\"
+if(!(test-path $logpath)){new-item -ItemType directory -path $logpath |out-null}
+#$width  = ([string]::Join("`n", (wmic path Win32_VideoController get CurrentHorizontalResolution))).split("`n") -match "\d{1,}"
+#$height  = ([string]::Join("`n", (wmic path Win32_VideoController get CurrentVerticalResolution))).split("`n") -match "\d{1,}"
 
-$width  = ([string]::Join("`n", (wmic path Win32_VideoController get CurrentHorizontalResolution))).split("`n") -match "\d{1,}"
-$height  = ([string]::Join("`n", (wmic path Win32_VideoController get CurrentVerticalResolution))).split("`n") -match "\d{1,}"
-
-  if(-not(test-path  $logpath)){new-item -ItemType directory -path $picpath |out-null}
-
+$results="OK"
+$Index="check $($logpath) result folder"
 
 if($result_type -match "passmark"){
 
@@ -43,46 +43,38 @@ if($result_type -match "passmark"){
   
   move-Item C:\testing_AI\logs\passmark*.log $logpath -Force
     move-Item C:\testing_AI\logs\passmark*.htm $logpath -Force
-  
-  $resultfilename= (Get-ChildItem $logpath -Filte "passmark*.log"|sort lastwritetime|select -last 1).fullname
-
-  $results="OK"
-  $Index= $resultfilename
-
-  }
+     }
 
   else{
-    $results="NG"
+  $results="NG"
   $Index= "No results. please check bitconfig settings"
-
   }
 
 }
 
 if($result_type -match "3dmark"){
 
-  $getlasttime=(Get-ChildItem -path C:\testing_AI\logs\logs_timemap.csv).lastwritetime
-  $nowtime=get-date
-  $gaps=(New-TimeSpan  -Start $getlasttime -end  $nowtime).TotalMinutes
+  #$getlasttime=(Get-ChildItem -path C:\testing_AI\logs\logs_timemap.csv).lastwritetime
+  #$nowtime=get-date
+  #$gaps=(New-TimeSpan  -Start $getlasttime -end  $nowtime).TotalMinutes
+  
   $Index=""
- 
-  $resultfile=Get-ChildItem -path $env:USERPROFILE\documents\3DMark\3DMark.log -ErrorAction SilentlyContinue
+   $resultfile=Get-ChildItem -path $env:USERPROFILE\documents\3DMark\3DMark.log -ErrorAction SilentlyContinue
    $resultfile2=Get-ChildItem -path $env:USERPROFILE\documents\3DMark\*.3dmark-result -ErrorAction SilentlyContinue
      $resultfile3=Get-ChildItem -path $env:USERPROFILE\documents\3DMark\*.xml -ErrorAction SilentlyContinue
        $resultfile32=Get-ChildItem -path $env:USERPROFILE\documents\*.xml -ErrorAction SilentlyContinue
-
 
    ## finish with log ###
 
   if($resultfile.count -ne 0){copy-Item  $env:USERPROFILE\documents\3DMark\3DMark.log $logpath -Force}
   if($resultfile2.count -ne 0){copy-Item $env:USERPROFILE\documents\3DMark\*.3dmark-result $logpath -Force}
   if($resultfile3.count -ne 0){copy-Item $env:USERPROFILE\documents\3DMark\*.xml $logpath -Force}
-  if($resultfile32.count -ne 0){copy-Item $env:USERPROFILE\documents\\*.xml $logpath -Force}
+  if($resultfile32.count -ne 0){copy-Item $env:USERPROFILE\documents\*.xml $logpath -Force}
 
-  $resultfilename=  [string]::join("`n",$resultfile.BaseName,$resultfile2.BaseName,$resultfile3.BaseName)
-
-  $results="OK"
-  $Index=$resultfilename
+  remove-item "$env:userprofile\Documents\3DMark\*.3dmark-result" -Force -ErrorAction SilentlyContinue
+  #remove-item "$env:userprofile\Documents\3DMark\3DMark.log" -Force -ErrorAction SilentlyContinue
+  remove-item "$env:userprofile\Documents\3DMark\*.xml" -Force -ErrorAction SilentlyContinue
+  remove-item "$env:userprofile\Documents\*.xml" -Force -ErrorAction SilentlyContinue
   
   }
   
