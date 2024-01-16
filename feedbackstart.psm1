a0e749165b2de28d89e446c719796b6a74207927
LazyFinger <97865400+106094@users.noreply.github.com>
2024-01-16 11:19:28 +0800

    add click again before tab to advance option

--- feedbackstart.psm1
+++ feedbackstart.psm1
@@ -1,9 +1,7 @@
-﻿
+﻿function feedbackstart([string]$para1,[string]$para2,[string]$para3,[string]$para4){
 
-function feedbackstart([string]$para1,[string]$para2,[string]$para3,[string]$para4){
-   
-  
- function Set-WindowState {
+#region import functions
+function Set-WindowState {
 	<#
 	.LINK
 	https://gist.github.com/Nora-Ballard/11240204
@@ -67,7 +65,8 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 		}
 	}
 }
- 
+ #endregion
+
 if($PSScriptRoot.length -eq 0){
 $scriptRoot="C:\testing_AI\modules"
 }
@@ -75,7 +74,6 @@ else{
 $scriptRoot=$PSScriptRoot
 }
  
-  
 Add-Type -AssemblyName System.Windows.Forms
 $nonlog_flag=$para4
 
@@ -86,7 +84,6 @@ $tcstep=((get-content $tcpath).split(","))[1]
 
 $picpath=(Split-Path -Parent $scriptRoot)+"\logs\$($tcnumber)\"
 if(-not(test-path $picpath)){new-item -ItemType directory -path $picpath |out-null}
-
 
     $actionss ="screenshot"
     Get-Module -name $actionss|remove-module
@@ -99,9 +96,12 @@ if(-not(test-path $picpath)){new-item -ItemType directory -path $picpath |out-nu
     $mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionpcai\b" -and $_.name -match "psm1"}).fullname
     Import-Module $mdpath -WarningAction SilentlyContinue
 
+    $actionclick ="mouse_action"
+    Get-Module -name $actionclick|remove-module
+    $mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionclick\b" -and $_.name -match "psm1"}).fullname
+    Import-Module $mdpath -WarningAction SilentlyContinue
 
 # 打开反馈中心应用程序
-
  
 <## reference ##
 Start-Process "feedback-hub:" -WindowStyle Maximized
@@ -216,7 +216,80 @@ foreach($list in $hashfortabcount.Keys){
     }
 }
 ##>
-    start-sleep -s 20
+$cSource3 = @'
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+public class Clicker3
+{
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct INPUT
+{ 
+    public int        type; // 0 = INPUT_MOUSE,
+                            // 1 = INPUT_KEYBOARD
+                            // 2 = INPUT_HARDWARE
+    public MOUSEINPUT mi;
+}
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646273(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct MOUSEINPUT
+{
+    public int    dx ;
+    public int    dy ;
+    public int    mouseData ;
+    public int    dwFlags;
+    public int    time;
+    public IntPtr dwExtraInfo;
+}
+
+//This covers most use cases although complex mice may have additional buttons
+//There are additional constants you can use for those cases, see the msdn page
+const int MOUSEEVENTF_MOVED      = 0x0001 ;
+const int MOUSEEVENTF_LEFTDOWN   = 0x0002 ;
+const int MOUSEEVENTF_LEFTUP     = 0x0004 ;
+const int MOUSEEVENTF_RIGHTDOWN  = 0x0008 ;
+const int MOUSEEVENTF_RIGHTUP    = 0x0010 ;
+const int MOUSEEVENTF_MIDDLEDOWN = 0x0020 ;
+const int MOUSEEVENTF_MIDDLEUP   = 0x0040 ;
+const int MOUSEEVENTF_WHEEL      = 0x0080 ;
+const int MOUSEEVENTF_XDOWN      = 0x0100 ;
+const int MOUSEEVENTF_XUP        = 0x0200 ;
+const int MOUSEEVENTF_ABSOLUTE   = 0x8000 ;
+
+const int screen_length = 0x10000 ;
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+[System.Runtime.InteropServices.DllImport("user32.dll")]
+extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+public static void LeftClickAtPoint(int x, int y)
+{
+    //Move the mouse
+    INPUT[] input = new INPUT[3];
+    input[0].mi.dx = x*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+    input[0].mi.dy = y*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+    input[0].mi.dwFlags = MOUSEEVENTF_MOVED | MOUSEEVENTF_ABSOLUTE;
+    //Left mouse button down
+    input[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    //Left mouse button up
+    input[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(3, input, Marshal.SizeOf(input[0]));
+}
+
+
+}
+'@
+Add-Type -TypeDefinition $cSource3 -ReferencedAssemblies System.Windows.Forms,System.Drawing
+
+$clickx=[int64]([System.Windows.Forms.Cursor]::Position.X)
+$clicky=[int64]([System.Windows.Forms.Cursor]::Position.Y)
+
+[Clicker3]::LeftClickAtPoint($clickx, $clicky)
+
+    start-sleep -s 2
     [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
     start-sleep -s 2
     [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
@@ -293,7 +366,7 @@ outlog $action $results $tcnumber $tcstep $index
     #$ws = New-Object -ComObject WScript.Shell  
     #$wsr = $ws.popup("Start recording with 3 minutes, Please reproduce issue",0,"Information",1 + 64)
 
-    echo "start recording"
+    write-output "start recording"
    
 
 }
