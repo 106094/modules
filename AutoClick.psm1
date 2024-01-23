04569c2cda037e7cad1c8891a57dbae8c0fd2486
isumi1357902 <41429695+isumi1357902@users.noreply.github.com>
2024-01-23 17:11:39 +0800

    123

---
+++ AutoClick.psm1
@@ -0,0 +1,47 @@
+
+function AutoClick() {
+    
+    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
+    #$wshell=New-Object -ComObject wscript.shell
+      Add-Type -AssemblyName Microsoft.VisualBasic
+      Add-Type -AssemblyName System.Windows.Forms
+        Add-Type -AssemblyName Microsoft.VisualBasic
+   
+   
+if($PSScriptRoot.length -eq 0){
+$scriptRoot="C:\testing_AI\modules"
+}
+else{
+$scriptRoot=$PSScriptRoot
+}
+
+$action="AutoClick"
+
+$tcpath=(Split-Path -Parent $scriptRoot)+"\currentjob\TC.txt"
+$tcnumber=((get-content $tcpath).split(","))[0]
+$tcstep=((get-content $tcpath).split(","))[1]
+
+$picpath=(Split-Path -Parent $scriptRoot)+"\logs\$($tcnumber)\"
+if(-not(test-path $picpath)){new-item -ItemType directory -path $picpath |out-null}
+
+#--------------------------------------------------------------------------------
+
+C:\testing_AI\modules\Auto-Click\Auto-Click.exe
+
+[System.Windows.Forms.SendKeys]::Sendwait("100");
+
+
+
+
+######### write log #######
+Get-Module -name "outlog"|remove-module
+$mdpath=(Get-ChildItem -path "C:\testing_AI\modules\"  -r -file |Where-object{$_.name -match "outlog" -and $_.name -match "psm1"}).fullname
+Import-Module $mdpath -WarningAction SilentlyContinue -Global
+
+#write-host "Do $action!"
+outlog $action $results $tcnumber $tcstep $index
+
+
+  }
+
+    export-modulemember -Function AutoClick
