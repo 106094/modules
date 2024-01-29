9491be0725d0d4f94911298fa27ab17915883bf3
isumi1357902 <41429695+isumi1357902@users.noreply.github.com>
2024-01-29 13:57:00 +0800

    123

--- TaskManager.psm1
+++ TaskManager.psm1
@@ -7,6 +7,84 @@ Function TaskManager([int]$para1,[int]$para2,[string]$para3){
     #Users
     #Details
     #Services
+    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
+    #$wshell=New-Object -ComObject wscript.shell
+    #$shell=New-Object -ComObject shell.application
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Windows.Forms,System.Drawing 
+
+
+    $clickSource = @'
+    using System;
+    using System.Drawing;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+    public class Clicker
+    {
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+    [StructLayout(LayoutKind.Sequential)]
+    struct INPUT
+    { 
+        public int        type; // 0 = INPUT_MOUSE,
+                                // 1 = INPUT_KEYBOARD
+                                // 2 = INPUT_HARDWARE
+        public MOUSEINPUT mi;
+    }
+    
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/ms646273(v=vs.85).aspx
+    [StructLayout(LayoutKind.Sequential)]
+    struct MOUSEINPUT
+    {
+        public int    dx ;
+        public int    dy ;
+        public int    mouseData ;
+        public int    dwFlags;
+        public int    time;
+        public IntPtr dwExtraInfo;
+    }
+    
+    //This covers most use cases although complex mice may have additional buttons
+    //There are additional constants you can use for those cases, see the msdn page
+    const int MOUSEEVENTF_MOVED      = 0x0001 ;
+    const int MOUSEEVENTF_LEFTDOWN   = 0x0002 ;
+    const int MOUSEEVENTF_LEFTUP     = 0x0004 ;
+    const int MOUSEEVENTF_RIGHTDOWN  = 0x0008 ;
+    const int MOUSEEVENTF_RIGHTUP    = 0x0010 ;
+    const int MOUSEEVENTF_MIDDLEDOWN = 0x0020 ;
+    const int MOUSEEVENTF_MIDDLEUP   = 0x0040 ;
+    const int MOUSEEVENTF_WHEEL      = 0x0080 ;
+    const int MOUSEEVENTF_XDOWN      = 0x0100 ;
+    const int MOUSEEVENTF_XUP        = 0x0200 ;
+    const int MOUSEEVENTF_ABSOLUTE   = 0x8000 ;
+    
+    const int screen_length = 0x10000 ;
+    
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+    
+    public static void LeftClickAtPoint(int x, int y)
+    {
+        //Move the mouse
+        INPUT[] input = new INPUT[3];
+        input[0].mi.dx = x*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+        input[0].mi.dy = y*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+        input[0].mi.dwFlags = MOUSEEVENTF_MOVED | MOUSEEVENTF_ABSOLUTE;
+        //Left mouse button down
+        input[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+        //Left mouse button up
+        input[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+        SendInput(3, input, Marshal.SizeOf(input[0]));
+    }
+    
+    
+    }
+'@
+Add-Type -TypeDefinition $clickSource -ReferencedAssemblies System.Windows.Forms,System.Drawing
+    
+    
+
 
     $paracheck1=$PSBoundParameters.ContainsKey('para1')
     $paracheck2=$PSBoundParameters.ContainsKey('para2')
@@ -117,17 +195,21 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 		}
 	}
 }
+    $actionss ="screenshot"
+    Get-Module -name $actionss|remove-module
+    $mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionss\b" -and $_.name -match "psm1"}).fullname
+    Import-Module $mdpath -WarningAction SilentlyContinue -Global
 
 
     Start-Process Taskmgr
 
     Start-Sleep -Seconds 10
-
-
-
     Get-Process -id (get-process -name "*Taskmgr*").Id | Set-WindowState -State MAXIMIZE
     #$taskid = Get-Process -id (get-process -name "*Taskmgr*").Id
 
+    &$actionss  -para3 nonlog -para5 "OpenTaskManager"
+
+    [Clicker]::LeftClickAtPoint(40, 10)  ## click
 
     if((Get-WmiObject -Class Win32_OperatingSystem).Caption -match "Windows 10"){        
         Start-Sleep -Seconds 10
@@ -169,11 +251,7 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
     }
     
     
-    $actionss ="screenshot"
-    Get-Module -name $actionss|remove-module
-    $mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionss\b" -and $_.name -match "psm1"}).fullname
-    Import-Module $mdpath -WarningAction SilentlyContinue -Global
-
+    
     if($selectTabPer){
         ##screenshot##
         &$actionss  -para3 nonlog -para5 $selectTab_$selectTabPer
