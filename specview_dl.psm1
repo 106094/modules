63489975419e89e9da98cf65b15017d6cc6101ce
LazyFinger <97865400+106094@users.noreply.github.com>
2024-02-01 11:37:30 +0800

    revised specview download

--- specview_dl.psm1
+++ specview_dl.psm1
@@ -70,6 +70,99 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 		}
 	}
 }
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Window {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+}
+
+public class Win32 {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+}
+
+public struct RECT {
+    public int Left;
+    public int Top;
+    public int Right;
+    public int Bottom;
+}
+"@
+
+$cSource = @'
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+public class Clicker
+{
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct INPUT
+{ 
+    public int        type; // 0 = INPUT_MOUSE,
+                            // 1 = INPUT_KEYBOARD
+                            // 2 = INPUT_HARDWARE
+    public MOUSEINPUT mi;
+}
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646273(v=vs.85).aspx
+[StructLayout(LayoutKind.Sequential)]
+struct MOUSEINPUT
+{
+    public int    dx ;
+    public int    dy ;
+    public int    mouseData ;
+    public int    dwFlags;
+    public int    time;
+    public IntPtr dwExtraInfo;
+}
+
+//This covers most use cases although complex mice may have additional buttons
+//There are additional constants you can use for those cases, see the msdn page
+const int MOUSEEVENTF_MOVED      = 0x0001 ;
+const int MOUSEEVENTF_LEFTDOWN   = 0x0002 ;
+const int MOUSEEVENTF_LEFTUP     = 0x0004 ;
+const int MOUSEEVENTF_RIGHTDOWN  = 0x0008 ;
+const int MOUSEEVENTF_RIGHTUP    = 0x0010 ;
+const int MOUSEEVENTF_MIDDLEDOWN = 0x0020 ;
+const int MOUSEEVENTF_MIDDLEUP   = 0x0040 ;
+const int MOUSEEVENTF_WHEEL      = 0x0080 ;
+const int MOUSEEVENTF_XDOWN      = 0x0100 ;
+const int MOUSEEVENTF_XUP        = 0x0200 ;
+const int MOUSEEVENTF_ABSOLUTE   = 0x8000 ;
+
+const int screen_length = 0x10000 ;
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+[System.Runtime.InteropServices.DllImport("user32.dll")]
+extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+public static void LeftClickAtPoint(int x, int y)
+{
+    //Move the mouse
+    INPUT[] input = new INPUT[3];
+    input[0].mi.dx = x*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+    input[0].mi.dy = y*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+    input[0].mi.dwFlags = MOUSEEVENTF_MOVED | MOUSEEVENTF_ABSOLUTE;
+    //Left mouse button down
+    input[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    //Left mouse button up
+    input[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(3, input, Marshal.SizeOf(input[0]));
+}
+}
+'@
+Add-Type -TypeDefinition $cSource -ReferencedAssemblies System.Windows.Forms,System.Drawing
 #endregion
 
     if($PSScriptRoot.length -eq 0){
@@ -121,8 +214,16 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
     $id=(Get-Process msedge |Where-object{($_.MainWindowTitle).length -gt 0}).Id
      start-sleep -s 2
      Get-Process -id $id | Set-WindowState -State MAXIMIZE
-
+     $Handle = Get-Process msedge| Where-Object { $_.MainWindowTitle -match $env:TITLE } | ForEach-Object { $_.MainWindowHandle }
+     if ( $Handle -is [System.Array] ) { $Handle = $Handle[0] }
+     $WindowRect = New-Object RECT
+     $GotWindowRect = [Window]::GetWindowRect($Handle, [ref]$WindowRect)
+     $clickx=($WindowRect.right)/2
+     $clicky= 20
+ 
      [Microsoft.VisualBasic.interaction]::AppActivate($id)|out-null
+     start-sleep -s 2
+     [Clicker]::LeftClickAtPoint($clickx, $clicky)
      start-sleep -s 2
      [System.Windows.Forms.SendKeys]::SendWait("{tab 10}")
      &$actionss  -para3 nonlog -para5 "start"
@@ -144,7 +245,7 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
      start-sleep -s 1
      [System.Windows.Forms.SendKeys]::SendWait("{tab 3}")
      &$actionss  -para3 nonlog -para5 "startdownload"
-     
+
      $starttime=Get-Date
 
      [System.Windows.Forms.SendKeys]::SendWait("~")
