0a1972a6a2199b2b878e70cf59894d93a5c7ff72
LazyFinger <97865400+106094@users.noreply.github.com>
2024-02-15 14:53:56 +0800

    openreg revised

--- openreg.psm1
+++ openreg.psm1
@@ -7,62 +7,62 @@ function openreg ([string]$para1,[string]$para2){
       Add-Type -AssemblyName System.Windows.Forms
    
 
-$paracheck=$PSBoundParameters.ContainsKey('para1')
+    $paracheck=$PSBoundParameters.ContainsKey('para1')
 
-if( $paracheck -eq $false -or $para1.length -eq 0 ){
-#write-host "no defined, setting 1 min after login"
-$para1="Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4.0"
-}
+    if( $paracheck -eq $false -or $para1.length -eq 0 ){
+    #write-host "no defined, setting 1 min after login"
+    $para1="Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4.0"
+    }
 
-$registryPath=$para1
-$nonlog_flag=$para2
+    $registryPath=$para1
+    $nonlog_flag=$para2
 
-if($PSScriptRoot.length -eq 0){
-$scriptRoot="C:\testing_AI\modules"
-}
-else{
-$scriptRoot=$PSScriptRoot
-}
+    if($PSScriptRoot.length -eq 0){
+    $scriptRoot="C:\testing_AI\modules"
+    }
+    else{
+    $scriptRoot=$PSScriptRoot
+    }
 
-$action="open reg with registryPath"
+    $action="open reg with registryPath"
 
-$tcpath=(Split-Path -Parent $scriptRoot)+"\currentjob\TC.txt"
-$tcnumber=((get-content $tcpath).split(","))[0]
-$tcstep=((get-content $tcpath).split(","))[1]
-$results="OK"
-$index="open reg with the path ok"
+    $tcpath=(Split-Path -Parent $scriptRoot)+"\currentjob\TC.txt"
+    $tcnumber=((get-content $tcpath).split(","))[0]
+    $tcstep=((get-content $tcpath).split(","))[1]
+    $results="OK"
+    $index="open reg with the path ok"
 
-$actionss ="screenshot"
-Get-Module -name $actionss|remove-module
-$mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionss\b" -and $_.name -match "psm1"}).fullname
-Import-Module $mdpath -WarningAction SilentlyContinue -Global
+    $actionss ="screenshot"
+    Get-Module -name $actionss|remove-module
+    $mdpath=(Get-ChildItem -path $scriptRoot -r -file |Where-object{$_.name -match "^$actionss\b" -and $_.name -match "psm1"}).fullname
+    Import-Module $mdpath -WarningAction SilentlyContinue -Global
 
-function jumpReg ($registryPath)
-{
-    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" `
-                     -Name "LastKey" `
-                     -Value $registryPath `
-                     -PropertyType String `
-                     -Force
+    function jumpReg ($registryPath)
+    {
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" `
+                        -Name "LastKey" `
+                        -Value $registryPath `
+                        -PropertyType String `
+                        -Force
 
-    start-process regedit -WindowStyle Maximized
-}
+        start-process regedit -WindowStyle Maximized
+    }
 
-try{
-jumpReg -registryPath $registryPath | Out-Null
-}
-catch{
-    $results="NG"
-    $index="fail to open reg with the path"
-}
+    try{
+    jumpReg -registryPath $registryPath | Out-Null
+    }
+    catch{
+        $results="NG"
+        $index="fail to open reg with the path"
+    }
 
-&$actionss -para3 nonlog -para5 "regpath"
+    &$actionss -para3 nonlog -para5 "regpath"
 
-(get-process -name regedit).CloseMainWindow()
+    (get-process -name regedit).CloseMainWindow()
 
-######### write log #######
+    ######### write log #######
 
-if($nonlog_flag.Length -eq 0){
+ if($nonlog_flag.Length -eq 0){
 
     Get-Module -name "outlog"|remove-module
     $mdpath=(Get-ChildItem -path "C:\testing_AI\modules\"  -r -file |Where-object{$_.name -match "outlog" -and $_.name -match "psm1"}).fullname
@@ -71,7 +71,7 @@ if($nonlog_flag.Length -eq 0){
     #write-host "Do $action!"
     outlog $action $results $tcnumber $tcstep $index
     
-}
+ }
 
   }
 
